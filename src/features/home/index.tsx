41d5762f4bcb64bd1579ae5da9f06c064ea3ba37
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-06-10 11:46:47 +0100

    Add details pages navigation

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,5 +1,5 @@
 import { Bell, LayoutGrid, Radio } from "lucide-react";
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet, useLocation } from "react-router";
 
 function Home() {
   const menuItems = [
@@ -8,25 +8,35 @@ function Home() {
     { name: "Tv Shows", to: "/tv-shows" },
   ];
 
+  const location = useLocation();
+
   return (
     <div className="flex h-screen w-full flex-col bg-[#0d0c0f] text-white">
       {/* Top Nav */}
       <header className="border-b border-[#151517] p-6 py-5">
         <nav className="flex justify-between">
           <ul className="text-fade flex gap-4 text-sm">
-            {menuItems.map((item) => (
-              <li>
-                <NavLink
-                  to={item.to}
-                  end
-                  className={({ isActive }) =>
-                    `font-medium transition-colors ${isActive ? "text-white" : "text-fade hover:text-zinc-300"}`
-                  }
-                >
-                  {item.name}
-                </NavLink>
-              </li>
-            ))}
+            {menuItems.map((item) => {
+              const isMoviesActive = item.to === "/" && location.pathname.includes("/movies/");
+
+              const isSeriesActive = item.to === "/series" && location.pathname.includes("/series/");
+
+              const isTvShowsActive = item.to === "/tv-shows" && location.pathname.includes("/tv-shows/");
+
+              return (
+                <li>
+                  <NavLink
+                    to={item.to}
+                    end
+                    className={({ isActive }) =>
+                      `font-medium transition-colors ${isActive || isMoviesActive || isSeriesActive || isTvShowsActive ? "text-white" : "text-fade hover:text-zinc-300"}`
+                    }
+                  >
+                    {item.name}
+                  </NavLink>
+                </li>
+              );
+            })}
           </ul>
           <ul className="flex gap-8 text-[#969899]">
             <li>
@@ -49,7 +59,7 @@ function Home() {
       </header>
 
       {/* Main Dashboard */}
-      <main className="scroll-container grid w-full scrollbar-none overflow-y-auto h-full p-6 py-0 pt-3 [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
+      <main className="scroll-container grid h-full w-full scrollbar-none overflow-y-auto p-6 py-0 pt-3 [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
         <Outlet />
       </main>
     </div>
