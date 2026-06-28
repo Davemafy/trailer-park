222e21a507ba87ed68905fa75cf54d8ec9cfd240
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-06-28 18:16:58 +0100

    Add sidebar provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router";
 import Home from "./features/home/";
-import  AppSidebar  from "./components/app-sidebar";
+import AppSidebar from "./components/app-sidebar";
 import Discovery from "./features/discovery/";
 import Community from "./features/community/";
 import ComingSoon from "./features/coming-soon/";
@@ -18,44 +18,46 @@ import TvShowsDetails from "./features/tv-shows-details";
 import SeriesDetails from "./features/series-details";
 import { RecentsProvider } from "./contexts/recents-context";
 import { BookmarksProvider } from "./contexts/bookmarks-context";
+import { SidebarProvider } from "./contexts/sidebar-context";
 
 function App() {
-  
   return (
     <>
-      <RecentsProvider>
-      <BookmarksProvider>
-        <div className="bg-[#0d0c0f] text-white">
-          {/* Navigation Menu */}
-          <div className="grid h-screen sm:grid-cols-[min-content_1fr]">
-            {/* Side Bar*/}
-            <AppSidebar />
-            <div className="h-screen">
-              {/* Route Configuration */}
-              <Routes>
-                <Route path="/" element={<Home />}>
-                  <Route index element={<Movies />} />
-                  <Route path="series" element={<Series />} />
-                  <Route path="tv-shows" element={<TvShows />} />
-                  <Route path="movies/:id" element={<MovieDetails />} />
-                  <Route path="tv-shows/:id" element={<TvShowsDetails />} />
-                  <Route path="series/:id" element={<SeriesDetails />} />
-                </Route>
-                <Route path="/discovery" element={<Discovery />} />
-                <Route path="/community" element={<Community />} />
-                <Route path="/coming-soon" element={<ComingSoon />} />
-                <Route path="/recents" element={<Recents />} />
-                <Route path="/bookmarks" element={<Bookmarks />} />
-                <Route path="/top-rated" element={<TopRated />} />
-                <Route path="/downloads" element={<Downloads />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/help" element={<Help />} />
-              </Routes>
+      <SidebarProvider>
+        <RecentsProvider>
+          <BookmarksProvider>
+            <div className="bg-[#0d0c0f] text-white">
+              {/* Navigation Menu */}
+              <div className="grid h-screen sm:grid-cols-[min-content_1fr]">
+                {/* Side Bar*/}
+                <AppSidebar />
+                <div className="h-screen">
+                  {/* Route Configuration */}
+                  <Routes>
+                    <Route path="/" element={<Home />}>
+                      <Route index element={<Movies />} />
+                      <Route path="series" element={<Series />} />
+                      <Route path="tv-shows" element={<TvShows />} />
+                      <Route path="movies/:id" element={<MovieDetails />} />
+                      <Route path="tv-shows/:id" element={<TvShowsDetails />} />
+                      <Route path="series/:id" element={<SeriesDetails />} />
+                    </Route>
+                    <Route path="/discovery" element={<Discovery />} />
+                    <Route path="/community" element={<Community />} />
+                    <Route path="/coming-soon" element={<ComingSoon />} />
+                    <Route path="/recents" element={<Recents />} />
+                    <Route path="/bookmarks" element={<Bookmarks />} />
+                    <Route path="/top-rated" element={<TopRated />} />
+                    <Route path="/downloads" element={<Downloads />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/help" element={<Help />} />
+                  </Routes>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </BookmarksProvider>
-      </RecentsProvider>
+          </BookmarksProvider>
+        </RecentsProvider>
+      </SidebarProvider>
     </>
   );
 }
