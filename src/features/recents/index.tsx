b1afb8b9e19fb9dfb919caa6a150ad9f8323d48a
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-07-13 03:36:45 +0100

    Update app pages navigation

--- src/features/recents/index.tsx
+++ src/features/recents/index.tsx
@@ -1,14 +1,42 @@
+import { Link } from "react-router";
 import { useRecents } from "./useRecents";
 import ContinueWatchingCard from "@/components/continue-watching-card";
+import { useSidebar } from "@/hooks/use-sidebar";
+import { Menu, X } from "lucide-react";
 
 const Recents = () => {
-  const { recents } = useRecents();
+  const { recents, clearRecents } = useRecents();
+  const { sidebarOpen, setSidebarOpen } = useSidebar();
+
   return (
-    <div className="p-6 py-4 grid gap-4">
-      <h1 className="font-semibold">Recently Viewed</h1>
-      <div className="flex flex-wrap gap-8">
-        {recents?.map((item) => (
-          <ContinueWatchingCard key={item.id} movie={item} handleWatchNow={() => alert("Incoming...")} />
+    <div className="flex h-full flex-col gap-4">
+      <div className="py- flex items-center justify-between border-b border-[#151517] p-6 py-4">
+        <h1 className="text-base">Recents</h1>
+        <button
+          onClick={() => clearRecents()}
+          className="hidden rounded-full bg-neutral-900 p-2 px-5 text-sm sm:block"
+        >
+          Clear Recents
+        </button>
+        <button className="block sm:hidden" onClick={() => setSidebarOpen(!sidebarOpen)}>
+          {!sidebarOpen && <Menu size={20} className="fill-[#9698P99] hover:fill-white" />}
+          {sidebarOpen && <X size={20} className="fill-[#9698P99] hover:fill-white" />}
+        </button>
+      </div>
+      <div className="flex h-full flex-wrap gap-4 overflow-y-auto p-6 py-0 [&::-webkit-scrollbar]:hidden">
+        {recents.length == 0 && (
+          <div className="xs:text-sm text-[13px] text-zinc-500">
+            No Recents yet, click the add movie button to add a bookmark.
+          </div>
+        )}
+        {recents.map((item) => (
+          <Link to={`/movies/${item.id}`}>
+            <ContinueWatchingCard
+              key={item.id}
+              movie={item}
+              handleWatchNow={() => alert("Incoming...")}
+            />
+          </Link>
         ))}
       </div>
     </div>
--- src/features/settings/index.tsx
+++ src/features/settings/index.tsx
@@ -1,8 +1,34 @@
+import { useSidebar } from "@/hooks/use-sidebar";
+import { Menu, X } from "lucide-react";
 
 const Settings = () => {
-  return (
-    <div className="p-6 py-4">Settings</div>
-  )
-}
+  const list = [];
+  const { sidebarOpen, setSidebarOpen } = useSidebar();
 
-export default Settings
+  return (
+    <div className="flex h-full flex-col gap-4">
+      <div className="flex items-center justify-between border-b border-[#151517] p-6 py-4">
+        <h1 className="text-base">Settings</h1>
+        <button
+          onClick={() => alert("Coming soon!")}
+          className="hidden rounded-full bg-neutral-900 p-2 px-5 text-sm sm:block"
+        >
+          Find your Settings
+        </button>
+        <button className="block sm:hidden" onClick={() => setSidebarOpen(!sidebarOpen)}>
+          {!sidebarOpen && <Menu size={20} className="fill-[#9698P99] hover:fill-white" />}
+          {sidebarOpen && <X size={20} className="fill-[#9698P99] hover:fill-white" />}
+        </button>
+      </div>
+      <div className="flex h-full flex-wrap gap-4 overflow-y-auto p-6 py-0 [&::-webkit-scrollbar]:hidden">
+        {list.length == 0 && (
+          <div className="xs:text-sm xs:text-sm text-xs text-zinc-500">
+            Nothing to see, click that button to find your settings.
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
