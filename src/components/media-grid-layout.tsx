2a5ddef9a4fc7dfb41eb4a06fd8d4be090766cb4
Davemafy <111160000+Davemafy@users.noreply.github.com>
2026-07-13 03:39:19 +0100

    Add skeleton loader

--- src/components/media-grid-layout.tsx
+++ src/components/media-grid-layout.tsx
@@ -1,9 +1,10 @@
-import { ChevronRight, Star } from "lucide-react";
+import { ChevronRight, Plus, Star } from "lucide-react";
 import ReactPlayer from "react-player";
 import TrendingCard from "./trending-card";
 import ContinueWatchingCard from "./continue-watching-card";
 import TopRatedCard from "./top-rated-card";
 import { Link, useLocation } from "react-router";
+import { Skeleton } from "./ui/skeleton";
 
 interface MediaGridLayoutProps {
   data: any;
@@ -24,13 +25,11 @@ export default function MediaGridLayout({ data, titlePrefix }: MediaGridLayoutPr
   } = data;
 
   if (loading) {
-    return <div className="text-zinc-500">Loading {titlePrefix.toLowerCase()} dashboard...</div>;
+    return <DemoLoader titlePrefix={titlePrefix} />;
   }
 
   const location = useLocation();
   const moviepath = `${location.pathname === "/" ? "movies" : location.pathname}`;
-
-  console.log(location.pathname, location);
 
   return (
     <div className="grid w-full gap-2">
@@ -75,6 +74,13 @@ export default function MediaGridLayout({ data, titlePrefix }: MediaGridLayoutPr
               <TrendingCard key={item.id} movie={item} handleWatchNow={handleWatchNow} />
             </Link>
           ))}
+          {trending.length == 0 && (
+            <>
+              <Skeleton className="bg-accent h-60 w-100 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-60 w-100 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-60 w-100 shrink-0 rounded-xl" />
+            </>
+          )}
         </div>
       </section>
 
@@ -82,14 +88,25 @@ export default function MediaGridLayout({ data, titlePrefix }: MediaGridLayoutPr
       <section className="grid w-full gap-3">
         <header className="flex justify-between">
           <h2 className="text-sm font-medium">Continue watching</h2>
-          <button className="text-fade flex items-center gap-1 text-[15px]">
+          <Link to={"/recents"} className="text-fade flex items-center gap-1 text-[15px]">
             See all <ChevronRight size={15} />
-          </button>
+          </Link>
         </header>
-        <div className="scroll-container -mx-6 flex min-h-50 gap-6 overflow-x-auto px-6 pb-4 [&::-webkit-scrollbar]:hidden">
+        <div className="scroll-container -mx-6 flex min-h-50 gap-3 overflow-x-auto px-6 pb-4 [&::-webkit-scrollbar]:hidden">
           {popular.slice(0, 10).map((item: any) => (
             <ContinueWatchingCard key={item.id} movie={item} handleWatchNow={handleWatchNow} />
           ))}
+
+          {popular.length == 0 && (
+            <>
+              <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+            </>
+          )}
         </div>
       </section>
 
@@ -109,6 +126,93 @@ export default function MediaGridLayout({ data, titlePrefix }: MediaGridLayoutPr
               <TopRatedCard key={item.id} movie={item} handleWatchNow={handleWatchNow} />
             </Link>
           ))}
+          {topRated.length == 0 && (
+            <>
+              <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+              <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+            </>
+          )}
+        </div>
+      </section>
+      <footer>
+        <div className="fixed right-6 bottom-6">
+          <Link
+            to={`/${titlePrefix.toLowerCase().replace(" ", "-")}/add`}
+            className="flex items-center gap-1 rounded-xl bg-red-600 p-2 px-2.5 text-sm"
+          >
+            Add{" "}
+            {titlePrefix.endsWith("series")
+              ? titlePrefix.toLocaleLowerCase()
+              : titlePrefix.slice(0, -1).toLowerCase()}{" "}
+            <Plus size={18} />
+          </Link>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+type DemoLoaderProps = {
+  titlePrefix: string;
+}
+
+function DemoLoader({ titlePrefix }:DemoLoaderProps) {
+  return (
+    <div className="grid w-full gap-2">
+      {/* Trending Loading Section */}
+      <section className="grid w-full gap-3">
+        <header className="flex justify-between">
+          <h2 className="text-sm font-medium">Trending {titlePrefix.toLowerCase()}</h2>
+          <button className="text-fade flex items-center gap-1 text-[15px]">
+            See all <ChevronRight size={15} />
+          </button>
+        </header>
+        <div className="scroll-container -mx-6 flex min-h-64 gap-2.5 overflow-x-auto px-6 pb-4 [&::-webkit-scrollbar]:hidden">
+          <Skeleton className="bg-accent h-60 w-100 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-60 w-100 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-60 w-100 shrink-0 rounded-xl" />
+        </div>
+      </section>
+
+      {/* Contrinue Watching Loading Section */}
+      <section className="grid w-full gap-3">
+        <header className="flex justify-between">
+          <h2 className="text-sm font-medium">Continue watching</h2>
+          <Link to={"/recents"} className="text-fade flex items-center gap-1 text-[15px]">
+            See all <ChevronRight size={15} />
+          </Link>
+        </header>
+        <div className="scroll-container -mx-6 flex min-h-50 gap-3 overflow-x-auto px-6 pb-4 [&::-webkit-scrollbar]:hidden">
+          <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-50 w-70 shrink-0 rounded-xl" />
+        </div>
+      </section>
+
+      {/* Top Rated Loading Section */}
+      <section className="grid w-full gap-3">
+        <header className="flex justify-between">
+          <h2 className="flex items-center gap-2 text-sm font-medium">
+            Top Rated <Star stroke="#ffc729" fill="#ffc729" size={16} />
+          </h2>
+          <button className="text-fade flex items-center gap-1 text-[15px]">
+            See all <ChevronRight size={15} />
+          </button>
+        </header>
+        <div className="scroll-container -mx-6 flex gap-3 overflow-x-auto px-6 pb-4 [&::-webkit-scrollbar]:hidden">
+          <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
+          <Skeleton className="bg-accent h-45 w-45 shrink-0 rounded-xl" />
         </div>
       </section>
     </div>
